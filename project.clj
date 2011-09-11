b0e88c26fa6cbde555efb648d008db0ab8ec71ee
Kyle S. Goodwin <kyle@devscoop.com>
2011-09-11 14:54:47 -0400

    Added new option :keywordize to control whether the client will keywordize the keys of maps when deserializing json. Switching json library to new clojure standard data.json contrib library and away from clj-json.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject goodwink/fleetdb-client "0.2.3"
+(defproject goodwink/fleetdb-client "0.3.0"
   :description "A Clojure client for FleetDB."
   :url "http://github.com/goodwink/fleetdb-client"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [clj-json "0.4.0"]]
+                 [org.clojure/data.json "0.1.1"]
+		 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[clj-unit "0.1.1"]])
--- src/fleetdb/client.clj
+++ src/fleetdb/client.clj
@@ -1,25 +1,28 @@
 (ns fleetdb.client
-  (:require [clj-json.core :as json])
+  (:require [clojure.data.json :as json])
   (:import (java.net Socket URI))
   (:import (java.io OutputStreamWriter BufferedWriter
                     InputStreamReader BufferedReader
                     Closeable))
   (:import (clojure.lang IFn ILookup)))
 
-(defn- doquery [#^BufferedWriter writer #^BufferedReader reader q]
-  (let [#^String req (json/generate-string q)]
+(defn- doquery [#^BufferedWriter writer #^BufferedReader reader q keywordize]
+  (let [#^String req (json/json-str q)]
     (if-let [resp (do (.write writer req)
                       (.write writer "\r\n")
                       (.flush writer)
                       (.readLine reader))]
-      (let [[status result] (json/parse-string resp true)]
+      (let [[status result] (json/read-json resp keywordize)]
         (if (zero? status)
           result
           (throw (Exception. #^String result))))
       (throw (Exception. "No response from server.")))))
 
-(defn query [client q]
-  (doquery (:writer client) (:reader client) q))
+(defn query
+  ([client q keywordize]
+     (doquery (:writer client) (:reader client) q keywordize))
+  ([client q]
+     (query client q false)))
 
 (defn close [client]
   (.close #^BufferedReader (:reader client))
@@ -38,20 +41,21 @@
 (defn connect [& [options]]
   (if-let [url (:url options)]
     (connect (apply-url url options))
-    (let [host     (get options :host "127.0.0.1")
-          port     (get options :port 3400)
-          timeout  (get options :timeout)
-          password (get options :password)
-          socket   (Socket. #^String host #^Integer port)
-          writer   (BufferedWriter. (OutputStreamWriter. (.getOutputStream  socket)))
-          reader   (BufferedReader. (InputStreamReader.  (.getInputStream   socket)))
-          attrs    {:writer writer :reader reader :socket socket
-                    :host host :port port :password password :timeout timeout}]
+    (let [host       (get options :host "127.0.0.1")
+          port       (get options :port 3400)
+          timeout    (get options :timeout)
+          password   (get options :password)
+	  keywordize (get options :keywordize)
+          socket     (Socket. #^String host #^Integer port)
+          writer     (BufferedWriter. (OutputStreamWriter. (.getOutputStream  socket)))
+          reader     (BufferedReader. (InputStreamReader.  (.getInputStream   socket)))
+          attrs      {:writer writer :reader reader :socket socket
+                      :host host :port port :password password :timeout timeout}]
       (when timeout
         (.setSoTimeout socket (int (* timeout 1000))))
       (when password
         (doquery writer reader ["auth" password]))
       (proxy [IFn ILookup Closeable] []
-        (invoke [q] (doquery writer reader q))
+        (invoke [q] (doquery writer reader q keywordize))
         (valAt  [k] (attrs k))
         (close  []  (close attrs))))))
